7f7bf3c62ee044c05af0ac255fc199e8329f7f45
GokuWeedLord <adam@notadam.me>
2017-11-17 21:54:29 +1100

    Add project files.

---
+++ ffxivpacketparser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ffxivpacketparser
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+    }
+}
